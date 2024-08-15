d317b9554b9eef802c76ed85b5558e73163d317e
Shsari <nyeo1998@gmail.com>
2024-08-15 16:08:24 +0900

    Initial Commit

---
+++ TouchRemote3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouchRemote3
+//
+//  Created by Seokhyun Song on 8/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
